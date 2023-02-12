7dd77f49fe9033dd0990bde147b9e0f3791600af
desivp77 <desivp93@gmail.com>
2023-02-12 22:41:01 +0200

    update auctions

--- src/components/auctions/Auctions.jsx
+++ src/components/auctions/Auctions.jsx
@@ -9,28 +9,6 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Card from "../card/Card.jsx";
 
-//  +++     Create an Auctions component that uses the 4 Card components inside
-// The Auctions component should receive a cards prop that receives an array of objects with this schema:
-// {
-//  "name": String,
-//  "user": {
-//     "avatar": String,
-//     "verified": Boolean
-//  },
-//  "mediaUrl": String,
-//  "price": String,
-//  "currency": String,
-//  "timeLeft": Number
-// }
-// +++     The default value of the cards prop must be an empty array
-// +++     It should also have a heading with the text "Live Auctions" and a Select component from Material UI.
-
-// Requirements
-// +++     The Select component must be imported from @mui/material and used
-// +++     You should implement a Material UI's Grid system for displaying the 4 Card components side by side
-// You should also use the Material UI buttons and container.
-// +++     You must use theContainer component from Material UI, as shown in the Example component
-// The main difference between this component and the Trending component is that here the cards will be live
  
 export default function Auctions({ cards = []}) {
 
